88941ce13dc568b0c405b78646b78640b59f3620
Luiz Paulo <lindennerd@gmail.com>
2023-03-01 01:17:52 -0300

    added sequelize module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Sequelize } from './imports/sequelize';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), Sequelize],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/imports/sequelize.ts
@@ -0,0 +1,10 @@
+import { ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+export const Sequelize = SequelizeModule.forRootAsync({
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    dialect: 'sqlite',
+    storage: configService.getOrThrow('DATABASE_CONNECTION'),
+  }),
+});
