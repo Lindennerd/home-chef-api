cb94e0139ba3281aac87de73699dc47a65b0302a
Luiz Paulo <lindennerd@gmail.com>
2023-03-04 07:23:58 -0300

    feature logout

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('health')
 export class AppController {
-  constructor() {}
-
   @Get()
   getHello(): string {
     return 'Hello World!';
